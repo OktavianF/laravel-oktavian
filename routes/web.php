044277759d2c40619cd69ee4e6cf981b9f2937b7
heyoktaaa <ofebrianto65@gmail.com>
2026-02-28 16:38:28 +0700

    menambahkan home dan about

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/about', [AboutController::class, 'index'])->name('about');
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
